78920ac2fc6b16261e73fa4276c8232c386aa067
uwi0 <kakapoweb@gmail.com>
2024-09-05 19:26:32 +0700

    creating FizzBuzzAlgModel

--- FizzBuzz/model/FizzBuzzAlgModel.swift
+++ FizzBuzz/model/FizzBuzzAlgModel.swift
@@ -1,8 +1,23 @@
-//
-//  FizzBuzzAlgModel.swift
-//  FizzBuzz
-//
-//  Created by dwi prasetyo on 05/09/24.
-//
-
 import Foundation
+
+struct FizzBuzzAlgModel: Identifiable {
+    let id = UUID()
+    let name: String
+    var commentCode: (String) -> String = { code in "\\" + code }
+    var versionNumber: Double? = nil
+    var imports: String = ""
+    var boilerWrapper: (String) -> String = { code in code }
+    let maxConsrDefz: String
+    let loopWrapper: (String) -> String
+    let printString: (String) -> String
+    let printInt: () -> String
+    var quality: String = "=="
+    var moduloSymbol: String = "%"
+    let ifStatement: (String, String) -> String
+    let elseIfStatement: (String, String) -> String
+    let elseStatement: (String) -> String
+    
+    var actualFizzBuzzCode: String {
+        "Fizzbuzz in \(name)"
+    }
+}
